d7b3b804cc7f23ca21596a296d8cd8e5dc197125
vagrant <vagrant@vagrant.vm>
2022-07-05 16:52:18 +0000

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-        for idx in my_list:
-            print("{:d}".format(idx))
+        my_list = [1,2,3,4,5]
+            print("{:d}".format(my_list))
